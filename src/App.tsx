46009d44f321f6ac8244fd563d1877dfa8526856
William Griffith <201064366+WRGII@users.noreply.github.com>
2025-09-10 20:16:32 -0600

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useAuth } from "./hooks/useAuth";
 import { useProfile } from "./hooks/useProfile";
-import { useUserPlan, hasActivePlan } from "./hooks/useUserPlan";
+import { useUserPlan, hasActivePlan } from './hooks/useUserPlan'
 
 // Pages
 import LandingPage from "./pages/LandingPage";
