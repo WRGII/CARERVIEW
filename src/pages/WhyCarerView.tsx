7a581a02cc9e9668800b948f87baf7dbf546bc1d
William Griffith <201064366+WRGII@users.noreply.github.com>
2025-09-22 08:56:01 -0600

    Updated WhyCarerView.tsx

--- src/pages/WhyCarerView.tsx
+++ src/pages/WhyCarerView.tsx
@@ -244,7 +244,8 @@ export default function WhyCarerView() {
         <div className="pt-12 pb-20 text-center">
           <div className="flex flex-col items-center justify-center mb-8">
             <h1 className="text-5xl md:text-6xl font-extrabold tracking-tight text-slate-gray">
-              Why you need CarerView
+              Your Observations are Vital for Their Care Plan!
+
             </h1>
           </div>
 
