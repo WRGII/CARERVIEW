a771e79cd0474f8da6659ddd78f376c6fa2ecb18
William Griffith <201064366+WRGII@users.noreply.github.com>
2026-02-11 15:59:57 +1300

    Updated HashScroll.tsx

--- src/components/util/HashScroll.tsx
+++ src/components/util/HashScroll.tsx
@@ -2,19 +2,21 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 export default function HashScroll() {
-  const { hash } = useLocation();
+  const { pathname, hash } = useLocation();
 
   useEffect(() => {
-    if (!hash) return;
-    const id = hash.replace("#", "");
-    const el = document.getElementById(id);
-    if (el) {
-      // delay in case the page needs to mount child components first
-      requestAnimationFrame(() => {
-        el.scrollIntoView({ behavior: "smooth", block: "start" });
-      });
+    if (hash) {
+      const id = hash.replace("#", "");
+      const el = document.getElementById(id);
+      if (el) {
+        requestAnimationFrame(() => {
+          el.scrollIntoView({ behavior: "smooth", block: "start" });
+        });
+      }
+    } else {
+      window.scrollTo(0, 0);
     }
-  }, [hash]);
+  }, [pathname, hash]);
 
   return null;
 }
