b642c65491caa939352e3bd3bd6d2ded8eb5f3fc
William Griffith <201064366+WRGII@users.noreply.github.com>
2026-06-29 22:47:20 +1200

    Added UI_TEXT_PHASE_9_TRANSLATION_SEEDING.md

--- src/i18n/LocaleProvider.tsx
+++ src/i18n/LocaleProvider.tsx
@@ -5,7 +5,7 @@ import { LocaleContext } from './LocaleContext'
 import type { Locale, SupportedLocale } from './types'
 import { LOCALE_STORAGE_KEY } from './localeStorageKey'
 
-const LS_TRANS_VERSION = 'v9'
+const LS_TRANS_VERSION = 'v10'
 function lsTransKey(locale: Locale): string {
   return `cv_trans_${locale}_${LS_TRANS_VERSION}`
 }
